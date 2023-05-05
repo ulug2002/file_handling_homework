4cef91a297c6b488624966583b4f527ef4e7e53f
ulug2002 <ulugbek_it@mail.ru>
2023-05-05 12:30:53 +0300

    d4

--- file04.py
+++ file04.py
@@ -6,5 +6,13 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    lst = []
+    for i in data:
+        if i.isalpha():
+            lst.append(i)
+        if i =='\n':
+            lst.append('\n')
+    return lst
 # Read data from file
+f=open('txt_file/data04.txt').read()
+print(main(f))
