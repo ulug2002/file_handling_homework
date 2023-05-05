69d3973c816cd42009517a895567f1fc59ce0c1d
ulug2002 <ulugbek_it@mail.ru>
2023-05-05 12:32:21 +0300

    d6

--- file06.py
+++ file06.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    a = data.split('\n')
+    n = []
+    for i in a:
+
+        n.append(len(i))
+    return n
 # Read data from file
+f = open('txt_file/data06.txt').read()
+print(main(f))
