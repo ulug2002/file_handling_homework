5db4563765a05c583ea952af082530000bcb37de
ulug2002 <ulugbek_it@mail.ru>
2023-05-05 12:31:37 +0300

    d5

--- file05.py
+++ file05.py
@@ -6,5 +6,19 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    a = 0
+    for i in data:
+        if i.isalpha():
+            a +=1
+        if i == '\n':
+            a +=1
+    n = 0
+    for i in data:
+        if i.isdigit():
+            n +=1
+    
+    return [a,n]
     
 # Read data from file
+f = open('txt_file/data05.txt').read()
+print(main(f))
