6a7be88c9bd64076eca66ce9cf036a05031d8d2d
ulug2002 <ulugbek_it@mail.ru>
2023-05-05 12:35:04 +0300

    d10

--- file10.py
+++ file10.py
@@ -6,5 +6,11 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    x1=data.split('\n')
+    x2=[]
+    for i in x1:
+        
+        x2.append(len(i))
+    return max(x2) 
 
-# Read data from file
+print(main(open('txt_file/data10.txt').read()))
